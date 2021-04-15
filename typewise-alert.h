ea5e020b3d7f641031a5c4f38cba5f0342c5b6a1
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-04-15 21:03:50 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#define LOWER_LIMIT 0
+#define PASSIVE_COOLING_UPPER_LIMIT 35
+#define HI_ACTIVE_COOLING_UPPER_LIMIT 45
+#define MED_ACTIVE_COOLING_UPPER_LIMIT 40
 typedef enum {
   PASSIVE_COOLING,
   HI_ACTIVE_COOLING,
@@ -12,21 +16,34 @@ typedef enum {
   TOO_HIGH
 } BreachType;
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 
 typedef enum {
   TO_CONTROLLER,
-  TO_EMAIL
+  TO_EMAIL,
+  TO_CONSOLE
 } AlertTarget;
+
+typedef enum
+{
+  ALERT_FAILURE,
+  ALERT_SUCCESS
+}AlertRetStatus;
 
 typedef struct {
   CoolingType coolingType;
   char brand[48];
 } BatteryCharacter;
 
-void checkAndAlert(
+typedef struct {
+  double lowerLimit;
+  double upperLimit;
+}LimitDetail_st;
+
+LimitDetail_st classifyTemperatureBreach[] = {{LOWER_LIMIT, PASSIVE_COOLING_UPPER_LIMIT},{LOWER_LIMIT,HI_ACTIVE_COOLING_UPPER_LIMIT},{LOWER_LIMIT,MED_ACTIVE_COOLING_UPPER_LIMIT}};
+BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+AlertRetStatus checkAndAlert(
   AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
 
-void sendToController(BreachType breachType);
-void sendToEmail(BreachType breachType);
+AlertRetStatus sendToController(BreachType breachType);
+AlertRetStatus sendToEmail(BreachType breachType);
+AlertRetStatus(*AlertType[])(BreachType) = {sendToController, sendToEmail, sendToController};
