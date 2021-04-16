2ab6920f8c3fae7d925423a9dae8c5c87964ff7d
srinath-93 <77610922+srinath-93@users.noreply.github.com>
2021-04-16 08:37:20 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,19 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
+
+TEST_CASE("infers the breach is too low according to limits") {
+  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+}
+
+TEST_CASE("infers the breach is too high according which is too high") {
+  REQUIRE(inferBreach(42, 10, 40) == TOO_HIGH);
+}
+
+TEST_CASE("infers the breach is normal according which is too normal") {
+  REQUIRE(inferBreach(40, 5, 40) == NORMAL);
+}
+
 TEST_CASE("infers the no breach according to passive cooling limits and sending to Console") {
   BatteryCharacter batteryChar = {PASSIVE_COOLING, "NORMAL_PASSIVE_COOLING"};
   REQUIRE(checkAndAlert(TO_CONSOLE, batteryChar, 35) == ALERT_SUCCESS);
@@ -46,16 +59,4 @@ TEST_CASE("infers the high breach according to med active cooling limits and sen
 TEST_CASE("infers the low breach according to med active cooling limits and sending to controller") {
   BatteryCharacter batteryChar = {MED_ACTIVE_COOLING, "MED_ACTIVE_COOLING_LOW_BREACH"};
   REQUIRE(checkAndAlert(TO_CONTROLLER, batteryChar, -2) == ALERT_SUCCESS);
-}
-
-TEST_CASE("infers the breach is too low according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
-}
-
-TEST_CASE("infers the breach is too high according which is too high") {
-  REQUIRE(inferBreach(42, 10, 40) == TOO_HIGH);
-}
-
-TEST_CASE("infers the breach is normal according which is too normal") {
-  REQUIRE(inferBreach(40, 5, 40) == NORMAL);
 }
